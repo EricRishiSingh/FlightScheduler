61ea148d70742a45f511a7afc78a9cb881743c04
Eric Singh <esingh@live.com>
2020-06-21 22:14:07 -0400

    Added some exception handling

--- Transportly.FlightScheduler/Program.cs
+++ Transportly.FlightScheduler/Program.cs
@@ -10,14 +10,21 @@ namespace Transportly.FlightScheduler
     {
         private static void Main(string[] args)
         {
-            var flightScheduler = new FlightScheduler();
-            flightScheduler.LoadScheduleFromJson(File.ReadAllText("FlightSchedule.json"));
-            flightScheduler.PrintSchedule();
+            try
+            {
+                var flightScheduler = new FlightScheduler();
+                flightScheduler.LoadScheduleFromJson(File.ReadAllText("FlightSchedule.json"));
+                flightScheduler.PrintSchedule();
 
-            var orderBatch = new OrderBatch();
-            orderBatch.LoadOrdersFromJson(File.ReadAllText("Orders.json"));
-            orderBatch.AssignOrderToFlight(flightScheduler.Flights);
-            orderBatch.PrintOrderBatchScheduleInformation();
+                var orderBatch = new OrderBatch();
+                orderBatch.LoadOrdersFromJson(File.ReadAllText("Orders.json"));
+                orderBatch.AssignOrderToFlight(flightScheduler.Flights);
+                orderBatch.PrintOrderBatchScheduleInformation();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
 
             Console.ReadLine();
         }
